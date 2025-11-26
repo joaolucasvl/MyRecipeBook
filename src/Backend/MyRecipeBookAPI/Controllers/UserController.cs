ebc9710fd38b2870bca57d3f3536c84e096c18e3
joaolucasvl <joaovitorlucasdev@gmail.com>
2025-11-25 21:41:02 -0300

    Teste

---
+++ src/Backend/MyRecipeBookAPI/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBookAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // Criar EndPoint para registrar um novo usuário
+
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson),StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request)
+        {
+            return Created(); // Retornar código 201 - Created e Receber os dados do usuário
+        }
+
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MyRecipeBook.Communication.Requests;
+
+public class RequestRegisterUserJson
+{
+
+    // Registros que vamos receber
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
